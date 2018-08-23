35bf730e84f69feca5cdb94a54cde9615fbabf9f
Ravindra Gopal <ravigopal02@gmail.com>
2018-08-23 17:54:41 +0530

    test

--- app/src/main/java/com/example/chaitanya/travistest/MainActivity.java
+++ app/src/main/java/com/example/chaitanya/travistest/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String Hi = "";
+    String Hi = "",hiii="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
